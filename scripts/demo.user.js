afdb19b932f536dc2786cf9b85cfb5419ec841f8
Handong TAN <81862747+xiaoxstz@users.noreply.github.com>
2026-03-08 13:39:43 +0100

    Add self-invoking function to demo user script

--- scripts/demo.user.js
+++ scripts/demo.user.js
@@ -6,3 +6,8 @@
 // @updateURL    https://raw.githubusercontent.com/xiaoxstz/Blog/main/scripts/demo.meta.js
 // @downloadURL  https://raw.githubusercontent.com/xiaoxstz/Blog/main/scripts/demo.user.js
 // ==/UserScript==
+
+(function () {
+    'use strict';
+    console.log('script loaded');
+})();
