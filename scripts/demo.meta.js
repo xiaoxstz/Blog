4fd107399f233229eef809f37958bac85e48269a
Handong TAN <81862747+xiaoxstz@users.noreply.github.com>
2026-03-08 14:01:40 +0100

    Update metadata for user script

--- scripts/demo.meta.js
+++ scripts/demo.meta.js
@@ -1,9 +1,13 @@
 // ==UserScript==
-// @name         My Test Script
+// @name         Autoupdate Test
 // @namespace    https://example.com/
-// @version      0.0.1
+// @version      2026-03-09
 // @description  Test auto update
-// @match        https://www.google.com/*
+// @author       xiaoxstz
+// @match        https://www.google.com/
+// @run-at       document-idle
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @updateURL    https://raw.githubusercontent.com/xiaoxstz/Blog/main/scripts/demo.meta.js
 // @downloadURL  https://raw.githubusercontent.com/xiaoxstz/Blog/main/scripts/demo.user.js
+// @grant        none
 // ==/UserScript==
