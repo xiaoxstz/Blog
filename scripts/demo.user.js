a6302342a8e72385b622b92c5723d02415ea4eb0
Handong TAN <81862747+xiaoxstz@users.noreply.github.com>
2026-03-08 13:39:22 +0100

    Add demo.user.js UserScript template

---
+++ scripts/demo.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         My Test Script
+// @namespace    https://example.com/
+// @version      0.0.1
+// @match        https://www.google.com/*
+// @updateURL    https://raw.githubusercontent.com/xiaoxstz/Blog/main/scripts/demo.meta.js
+// @downloadURL  https://raw.githubusercontent.com/xiaoxstz/Blog/main/scripts/demo.user.js
+// ==/UserScript==
